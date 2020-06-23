b135ba23f96673792a6e2fb6c3defc93d7d1d0ce
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-06-23 12:32:17 -0600

    Backend questions added

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,9 @@ const { request } = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const questions = require("./routes/questions");
+const index = require("./routes/index");
 
-
+app.use("/", index);
 app.use("/questions", questions);
 
 app.listen(PORT, () => {
