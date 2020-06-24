f2737d37bf4a2502ce3b2db5489591db8ba2efed
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-06-24 14:24:12 -0600

    AUTH prart2 done

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const questionsUrl = "http://localhost:3000/questions";
 fetch(questionsUrl, {
     headers: 'Access-Control-Allow-Origin'
 })
-    .then(parseJSON)
+    // .then(parseJSON)
     .then(console.log)
 
 function parseJSON(response){
