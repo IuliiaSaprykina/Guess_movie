b400ca9c2da247832b2248820de7ce625372fe90
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-06-25 20:53:52 -0600

    The game has been started

--- frontend/gamePage.js
+++ frontend/gamePage.js
@@ -1,79 +1,43 @@
-const questionsUrl = "http://localhost:3000/questions/";
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const qImg = document.getElementById("movie-img");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const progress = document.getElementById("progress");
-const scoreContainer = document.getElementById("scoreContainer");
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container')
+const questionsUrl = "http://localhost:3000/questions/"
 
-fetch(questionsUrl, {
-    method: "GET", 
-    headers: {
-        "Content-type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`
-    }
-})
-    .then(response => response.json())
-    .then(result => shuffleQuestions(result.questions))
-    // .then(console.log)
+startButton.addEventListener('click', startGame)
 
-    
-
-function shuffleQuestions(questions) {
-    let j, x, i;
-    for (i = questions.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = questions[i];
-        questions[i] = questions[j];
-        questions[j] = x;
-    }
-    return questions
+function startGame() {
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    console.log(questions())
+    // setNextQuestion()
 }
 
-// console.log(shuffleQuestions())
+function nextQuestion(){
 
-// let questions = shuffleQuestions();
+}
 
-// function startGame(questions) {
-//     let lastQuestionIndex = questions.length - 1;
-//     let runningQuestionIndex = 0;
-//     renderQuestion(lastQuestionIndex, runningQuestionIndex, questions)
-//     progressRender(lastQuestionIndex, runningQuestionIndex, questions)
-// } 
+function selectAnswer(){
 
-// function renderQuestion(lastQuestionIndex, runningQuestionIndex, questions){
-//     let q = questions[runningQuestionIndex];
-//     qImg.src = q.shot_src;
-//     choiceA.textContent = q.choiceA;
-//     choiceB.textContent = q.choiceB;
-//     choiceC.textContent = q.choiceC;
-//     choiceD.textContent = q.choiceD;
- 
-// }
+}
 
-// startGame(questions)
-
-// function startGame(questions) {
-//     let lastQuestionIndex = questions.length - 1;
-//     let runningQuestionIndex = 0;
-//     renderQuestion(lastQuestionIndex, runningQuestionIndex, questions)
-//     progressRender(lastQuestionIndex, runningQuestionIndex, questions)
-// }
-
-// function renderQuestion(lastQuestionIndex, runningQuestionIndex, questions){
-//     let q = questions[runningQuestionIndex];
-//     qImg.src = q.shot_src;
-//     choiceA.textContent = q.choiceA;
-//     choiceB.textContent = q.choiceB;
-//     choiceC.textContent = q.choiceC;
-//     choiceD.textContent = q.choiceD;
-         
-// }
-
-
+function questions () {
+    fetch(questionsUrl, {
+        headers: {
+            Authorization: `bearer ${localStorage.getItem("token")}`,
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        const allQuestions = result.questions
+        console.log(allQuestions)
+        let j, x, i;
+        for (i = allQuestions.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = allQuestions[i];
+            allQuestions[i] = allQuestions[j];
+            allQuestions[j] = x;
+        }
+    return allQuestions
+    })
+    
+}
 
